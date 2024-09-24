c5235940fffb3e15ba4e8510490af8d236c8c015
maxulysse <max.u.garcia@gmail.com>
2024-09-24 15:24:26 +0200

    add removeNextflowVersion function

--- src/main/java/nf_core/nf/test/Methods.java
+++ src/main/java/nf_core/nf/test/Methods.java
@@ -1,5 +1,13 @@
 package nf_core.nf.test.utils;
 
 public class Methods {
+  // Remove Nextflow version from pipeline_software_mqc_versions.yml
+  public static Object removeNextflowVersion(file pipeline_software_mqc_versions) {
+    def softwareVersions = path(pipeline_software_mqc_versions).yaml;
+    if (softwareVersions.containsKey("Workflow")) {
+      softwareVersions.Workflow.remove("Nextflow");
+    }
+    return softwareVersions;
+  }
 
 }
