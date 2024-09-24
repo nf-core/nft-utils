0b651fcf203de02755deff6a1f5d3d86e9df9313
maxulysse <max.u.garcia@gmail.com>
2024-09-24 18:10:02 +0200

    update syntax

--- src/main/java/nf_core/nf/test/Methods.java
+++ src/main/java/nf_core/nf/test/Methods.java
@@ -1,13 +1,19 @@
 package nf_core.nf.test.utils;
 
+import org.yaml.snakeyaml.Yaml;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Methods {
+
   // Remove Nextflow version from pipeline_software_mqc_versions.yml
-  public static Object removeNextflowVersion(file pipeline_software_mqc_versions) {
-    def softwareVersions = path(pipeline_software_mqc_versions).yaml;
-    if (softwareVersions.containsKey("Workflow")) {
-      softwareVersions.Workflow.remove("Nextflow");
+  public static removeNextflowVersion(Path softwareVersions) {
+    final Yaml softwareVersionsYaml = path(softwareVersions);
+    if (softwareVersionsYaml.containsKey("Workflow")) {
+      softwareVersionsYaml.Workflow.remove("Nextflow");
     }
-    return softwareVersions;
+    return removeNextflowVersion(softwareVersionsYaml);
   }
 
 }
