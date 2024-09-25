790027a6c4fd645a1b69bf9a47084b30cc07aae4
maxulysse <max.u.garcia@gmail.com>
2024-09-25 12:47:45 +0200

    output as Map

--- src/main/java/nf_core/nf/test/utils/Methods.java
+++ src/main/java/nf_core/nf/test/utils/Methods.java
@@ -10,11 +10,11 @@ import java.util.Map;
 public class Methods {
 
   // Remove Nextflow version from pipeline_software_mqc_versions.yml
-  public static CharSequence removeNextflowVersion(CharSequence softwareVersionsFile)
+  public static Map<String, Object> removeNextflowVersion(CharSequence softwareVersionsFile)
       throws URISyntaxException, MalformedURLException, IOException {
     final Map<String, Object> softwareVersionsMap = new Yaml().load(softwareVersionsFile.toString());
     softwareVersionsMap.remove("Workflow", "Nextflow");
 
-    return removeNextflowVersion(softwareVersionsMap.toString());
+    return softwareVersionsMap;
   }
 }
