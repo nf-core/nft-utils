40e65fb1ea82c52f4ed4a6bf33dc69272795e988
maxulysse <max.u.garcia@gmail.com>
2024-09-25 10:29:01 +0200

    input is CharSequence

--- src/main/java/nf_core/nf/test/Methods.java
+++ src/main/java/nf_core/nf/test/Methods.java
@@ -6,8 +6,8 @@ import java.util.Map;
 public class Methods {
 
   // Remove Nextflow version from pipeline_software_mqc_versions.yml
-  public static String removeNextflowVersion(String softwareVersionsFile) {
-    final Map<String, Object> softwareVersionsMap = new Yaml().load(softwareVersionsFile);
+  public static CharSequence removeNextflowVersion(CharSequence softwareVersionsFile) {
+    final Map<String, Object> softwareVersionsMap = new Yaml().load(softwareVersionsFile.toString());
     softwareVersionsMap.remove("Workflow", "Nextflow");
 
     return softwareVersionsMap.toString();
