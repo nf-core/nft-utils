91fd739f3872c2a28af91c10960d2723a707764f
maxulysse <max.u.garcia@gmail.com>
2024-10-01 18:33:09 +0200

    RC 0.0.2

--- bump-version.sh
+++ bump-version.sh
@@ -7,7 +7,7 @@ fi
 sed -i -e "0,/<version>/{s/<version>.*<\\/version>/<version>$version<\\/version>/}" ./pom.xml
 sed -i -e "s/moduleVersion=.*/moduleVersion=$version/" ./src/main/resources/META-INF/nf-test-plugin
 
-if [[ $version != *dev ]]
+if [ $version != *dev ]
 then
     sed -i -e "s/load \"nft-utils@.*\"/load \"nft-utils@$version\"/" ./docs/index.md
 fi
