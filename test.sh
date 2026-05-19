7497cccc80fa39876c0c90577125f87dfc2f0850
Maxime U Garcia <max.u.garcia@gmail.com>
2026-05-19 19:35:32 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ] || [[ ! "$*" == *"tests/"* ]]; then
-    nf-test test --plugins target/nft-utils-*.jar --verbose --debug tests/ --update-snapshot --clean-snapshot
+    nf-test test --plugins target/nft-utils-*.jar --verbose --debug tests/
 else
-    nf-test test --plugins target/nft-utils-*.jar --verbose --debug ${@} --update-snapshot --clean-snapshot
+    nf-test test --plugins target/nft-utils-*.jar --verbose --debug ${@}
 fi
